afc1d64f65dd45525231e3678bf50ddc10f4122b
aakash <aakashsharma0810@gmail.com>
2021-03-04 09:19:11 +0530

    first commit for this repo

---
+++ src/test/java/aka/AppTest.java
@@ -0,0 +1,29 @@
+package aka;
+import org.testng.annotations.Test;
+
+public class AppTest
+
+
+
+{
+	
+	
+	@Test
+	public void method1()
+	
+	{
+		
+		System.out.println("aakash");
+		
+	}
+	
+	@Test(enabled=false)
+	public void method2()
+	
+	{
+		
+		System.out.println("aakash");
+		
+	}
+	
+}
